46e103934809b5f15d3f14dcff43bef99bbfc3c1
prettyPro <862669640@qq.com>
2018-07-27 13:52:46 +0800

    run

--- test.py
+++ test.py
@@ -1,48 +1,47 @@
 import os
+import librosa # pip install librosa
+from tqdm import tqdm # pip install tqdm 显示进度条
+import tensorflow as tf
+import numpy as np
 
 
-def get_wav_files(parent_dir,  sub_dirs):
+# 获得训练用的wav文件路径列表
+def get_wav_files(parent_dir, sub_dirs):
     wav_files = []
-    file = open("labels.txt", "w")
-    for l,  sub_dir in enumerate(sub_dirs):
-        wav_path = os.sep.join([parent_dir,  sub_dir])
-        for (dirpath,  dirnames,  filenames) in os.walk(wav_path):
-            # print("filenames:%s"%(filenames))
+    for l, sub_dir in enumerate(sub_dirs):
+        wav_path = os.path.join(parent_dir, sub_dir)
+        for (dirpath, dirnames, filenames) in os.walk(wav_path):
             for filename in filenames:
-                # print("filename:%s"%(filename))
-                # if filename.endswith('.wav') or filename.endswith('.WAV'):
-                filename_path = os.sep.join([dirpath,  filename])
-                # print("filename_path:%s" % (filename_path))
-                wav_files.append(filename_path)#filename_path:audio\5\3xing\xing020.wav
-                label_temp = filename_path.split('\\')
-                label = label_temp[1] + label_temp[2][0]
-                label_pair = filename_path + " " + label
-                file.write(label_pair + "\n")
-    file.close()
-    print("finished!")
-
-def create():
-    file = open("labels.txt", "r")
-    label_pair = open("label_pair.txt", "w")
-    label_set = []
-    labels = file.readlines()
-    for x in labels:
-        pairs = x.split(" ")
-        a = pairs[1].rstrip("\n")
-        if a not in label_set:
-            label_set.append(a)
-    for y in labels:
-        pairs = y.split(" ")
-        a1 = pairs[0]
-        a2 = pairs[1].rstrip("\n")
-        a2_index = label_set.index(a2)
-        s = a1 + " " + str(a2_index) + "\n"
-        label_pair.write(s)
-    file.close()
-    label_pair.close()
+                if filename.endswith('.wav') or filename.endswith('.WAV'):
+                    filename_path = os.sep.join([dirpath, filename])
+                    wav_files.append(filename_path)
+    return wav_files
 
 
+# 获取文件mfcc特征和对应标签
+def extract_features(wav_files):
+    inputs = []
+    labels = []
+
+    for wav_file in tqdm(wav_files):
+        # 读入音频文件
+        audio, fs = librosa.load(wav_file)
+
+        # 获取音频mfcc特征
+        # [n_steps, n_inputs]
+        mfccs = np.transpose(librosa.feature.mfcc(y=audio, sr=fs, n_mfcc=FLAGS.n_inputs), [1, 0])
+        inputs.append(mfccs.tolist())
+        # 获取label
+    for wav_file in wav_files:
+        label = wav_file.split('/')[-1].split('-')[1]
+        labels.append(label)
+    return inputs, np.array(labels, dtype=np.int)
 
 if __name__ == '__main__':
-    # get_wav_files("audio","1,2,3,4,5")
-    create()
+    # 获得训练用的wav文件路径列表
+    wav_files = get_wav_files("audio", "1,2,3,4,5")
+    # 获取文件mfcc特征和对应标签
+    tr_features,tr_labels = extract_features(wav_files)
+
+    np.save('tr_features.npy',tr_features)
+    np.save('tr_labels.npy',tr_labels)
